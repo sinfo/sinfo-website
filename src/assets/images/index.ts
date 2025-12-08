a9ca1010a32bb3faa8b9b9b56e45c9d9ef20fd29
Francisca Almeida <francisca.vicente.de.almeida@tecnico.ulisboa.pt>
2025-12-08 00:18:20 +0000

    refactor: work hacky image name

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,3 @@
 export { default as hackyPeeking } from "./hacky_404.svg";
-export { default as workHacky } from "./Work Hacky.png";
+export { default as workHacky } from "./hacky_work.png";
 export { default as sinfoLogo } from "./sinfo-logo.svg";
