8393e1e3ed954dcbab9c5065dfff764d4aadc5cb
Francisca Vicente de Almeida <francisca.vicente.de.almeida@tecnico.ulisboa.pt>
2026-03-19 12:58:08 +0000

    feat: add Privacy Policy page with comprehensive user information guidelines

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,238 @@
+export default function PrivacyPage() {
+  return (
+    <main className="min-h-screen bg-gray-100">
+      <section className="bg-gradient-to-br from-sinfo-primary via-sinfo-primary to-sinfo-secondary py-16 sm:py-20 md:py-24">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="text-center">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-white mb-4 sm:mb-6">
+              Privacy Policy
+            </h1>
+          </div>
+        </div>
+      </section>
+
+      <section className="py-12">
+        <div className="max-w-7xl mx-auto px-6">
+          <p>
+            This Privacy Policy governs the manner in which SINFO collects,
+            uses, maintains and discloses information collected from users
+            (each, a &quot;User&quot;) of the http://sinfo.org website
+            (&quot;Site&quot;).
+          </p>
+          <br />
+
+          <h3>Personal identification information</h3>
+          <p>
+            We may collect personal identification information from Users in a
+            variety of ways, including, but not limited to, when Users visit our
+            site, register on the site, and in connection with other activities,
+            services, features or resources we make available on our Site. Users
+            may be asked for, as appropriate, name, email address, mailing
+            address, phone number. Users may, however, visit our Site
+            anonymously. We will collect personal identification information
+            from Users only if they voluntarily submit such information to us.
+            Users can always refuse to supply personally identification
+            information, except that it may prevent them from engaging in
+            certain Site related activities.
+          </p>
+
+          <br />
+
+          <h3>Non-personal identification information</h3>
+          <p>
+            We may collect non-personal identification information about Users
+            whenever they interact with our Site. Non-personal identification
+            information may include the browser name, the type of computer and
+            technical information about Users means of connection to our Site,
+            such as the operating system and the Internet service providers
+            utilized and other similar information.
+          </p>
+
+          <br />
+
+          <h3>Web browser cookies</h3>
+          <p>
+            Our Site may use &quot;cookies&quot; to enhance User experience.
+            User&apos;s web browser places cookies on their hard drive for
+            record-keeping purposes and sometimes to track information about
+            them. User may choose to set their web browser to refuse cookies, or
+            to alert you when cookies are being sent. If they do so, note that
+            some parts of the Site may not function properly.
+          </p>
+
+          <br />
+
+          <h3>How we use collected information</h3>
+          <p>
+            SINFO may collect and use Users personal information for the
+            following purposes:
+          </p>
+          <br />
+
+          <ul>
+            <li>
+              <b>To run and operate our Site</b>
+              <br />
+              We may need your information display content on the Site
+              correctly.
+            </li>
+            <br />
+            <li>
+              <b>To improve customer service</b>
+              <br /> The information you provide helps us respond to your
+              customer service requests and support needs more efficiently.
+            </li>
+            <br />
+            <li>
+              <b>To personalize user experience</b>
+              <br /> We may use information in the aggregate to understand how
+              our Users as a group use the services and resources provided on
+              our Site.
+            </li>
+            <br />
+            <li>
+              <b>To improve our Site</b>
+              <br /> We may use feedback you provide to improve our products and
+              services.
+            </li>
+            <br />
+            <li>
+              <b>To run a promotion, contest, survey or other</b>
+              <br /> Site feature We send Users information they agreed to
+              receive about topics we think will be of interest to them.
+            </li>
+            <br />
+            <li>
+              <b>To send periodic emails</b>
+              <br /> We may use the email address to send User information and
+              updates pertaining to their order. It may also be used to respond
+              to their inquiries, questions, and/or other requests.
+            </li>
+          </ul>
+
+          <br />
+
+          <h3>How we protect your information</h3>
+          <p>
+            We adopt appropriate data collection, storage and processing
+            practices and security measures to protect against unauthorized
+            access, alteration, disclosure or destruction of your personal
+            information, username, password, transaction information and data
+            stored on our Site.
+          </p>
+
+          <br />
+
+          <h3>
+            Sharing your non-personal information with third-party services
+          </h3>
+          <p>
+            We may use third party service providers to help us operate our
+            business and the Site or administer activities on our behalf, such
+            as sending out newsletters or surveys. We may share information with
+            these third parties for those limited purposes provided that you
+            have given us your permission.
+          </p>
+
+          <br />
+
+          <h3>Sharing your personal information with sponsors</h3>
+          <p>
+            We may provide the event sponsors the personal information the User
+            explicitly grant us to share with them — specifically, the
+            User&apos;s submitted curriculum and its contents; and name, e-mail,
+            and/or phone number, in case it has been explicitly authorized its
+            sharing with sponsors companies amid the event. The User can ask at
+            any time for a copy of the collected information, as well as its
+            removal from the database.
+          </p>
+
+          <br />
+
+          <h3>Electronic newsletters</h3>
+          <p>
+            If User decides to opt-in to our mailing list, they will receive
+            emails that may include company news, updates, related product or
+            service information, etc. If at any time the User would like to
+            unsubscribe from receiving future emails, we include detailed
+            unsubscribe instructions at the bottom of each email or User may
+            contact us via our Site. We may use third party service providers to
+            help us operate our business and the Site or administer activities
+            on our behalf, such as sending out newsletters or surveys. We may
+            share your information with these third parties for those limited
+            purposes provided that you have given us your permission. We will
+            not share the User&apos;s personal email with sponsors nor with
+            other third-parties besides the ones aforementioned.
+          </p>
+
+          <br />
+
+          <h3>Third party websites</h3>
+          <p>
+            Users may find advertising or other content on our Site that link to
+            the sites and services of our partners, suppliers, advertisers,
+            sponsors, licensors and other third parties. We do not control the
+            content or links that appear on these sites and are not responsible
+            for the practices employed by websites linked to or from our Site.
+            In addition, these sites or services, including their content and
+            links, may be constantly changing. These sites and services may have
+            their own privacy policies and customer service policies. Browsing
+            and interaction on any other website, including websites which have
+            a link to our Site, is subject to that website&apos;s own terms and
+            policies.
+          </p>
+
+          <br />
+
+          <h3>Image and video coverage</h3>
+          <p>
+            We may capture image and video content throughout the event&apos;s
+            sessions. We may share this material within our social networks, and
+            we may also share it with our sponsors. By attending the event and
+            using this site, the User grants all rights to SINFO to use this
+            material in perpetuity. The User, however, can at any time ask for a
+            copy of the collected image and video coverage and request the
+            permanent deletion of a media item in which the User is present or
+            can be directly or indirectly identified.
+          </p>
+
+          <br />
+
+          <h3>Changes to this privacy policy</h3>
+          <p>
+            SINFO has the discretion to update this privacy policy at any time.
+            When we do, we will post a notification on the main page of our
+            Site. We encourage Users to frequently check this page for any
+            changes to stay informed about how we are helping to protect the
+            personal information we collect. You acknowledge and agree that it
+            is your responsibility to review this privacy policy periodically
+            and become aware of modifications.
+          </p>
+
+          <br />
+
+          <h3>Your acceptance of these terms</h3>
+          <p>
+            By using this Site and attending our event, you signify your
+            acceptance of this policy. If you do not agree to this policy,
+            please do not use our Site nor attend our event. Your continued use
+            of the Site and event attendance following the posting of changes to
+            this policy will be deemed your acceptance of those changes.
+          </p>
+
+          <br />
+
+          <h3>Contacting us</h3>
+          <p>
+            If you have any questions about this Privacy Policy, the practices
+            of this site, or your dealings with this site, please contact us.
+          </p>
+
+          <br />
+          <br />
+          <p>This document was last updated on March 19, 2026.</p>
+        </div>
+      </section>
+    </main>
+  );
+}
